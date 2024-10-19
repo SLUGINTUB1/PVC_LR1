0f38f945309b5197426bacf04b64be8e2b1d8d1b
SLUGINTUB1 <max2003pas@gmail.com>
2024-10-20 00:30:31 +0300

    Add project files.

---
+++ LR1/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+using LR1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LR1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
+        public IActionResult BlogDetails()
+        {
+            return View();
+        }
+
+        public IActionResult PortfolioDetails()
+        {
+            return View();
+        }
+
+        public IActionResult ServicesDetails()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
